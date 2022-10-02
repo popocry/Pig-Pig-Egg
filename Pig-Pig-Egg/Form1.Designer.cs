9fb96303ee0994bff87590bfc45e737ebfb292dd
simon880525 <simon880525@gmail.com>
2022-10-02 12:38:05 +0800

    初版

--- Pig-Pig-Egg/Form1.Designer.cs
+++ Pig-Pig-Egg/Form1.Designer.cs
@@ -29,13 +29,137 @@ namespace Pig_Pig_Egg
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbquestion = new System.Windows.Forms.Label();
+            this.lbscore = new System.Windows.Forms.Label();
+            this.lbans = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbresult = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 274);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbquestion
+            // 
+            this.lbquestion.AutoSize = true;
+            this.lbquestion.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbquestion.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbquestion.Location = new System.Drawing.Point(101, 66);
+            this.lbquestion.Name = "lbquestion";
+            this.lbquestion.Size = new System.Drawing.Size(851, 36);
+            this.lbquestion.TabIndex = 2;
+            this.lbquestion.Text = "Which image was taken by the James Webb Space Telescope?";
+            // 
+            // lbscore
+            // 
+            this.lbscore.AutoSize = true;
+            this.lbscore.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbscore.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbscore.Location = new System.Drawing.Point(916, 19);
+            this.lbscore.Name = "lbscore";
+            this.lbscore.Size = new System.Drawing.Size(56, 36);
+            this.lbscore.TabIndex = 4;
+            this.lbscore.Text = "0/4";
+            // 
+            // lbans
+            // 
+            this.lbans.AutoSize = true;
+            this.lbans.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbans.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbans.Location = new System.Drawing.Point(423, 474);
+            this.lbans.Name = "lbans";
+            this.lbans.Size = new System.Drawing.Size(70, 36);
+            this.lbans.TabIndex = 5;
+            this.lbans.Text = "N/A";
+            this.lbans.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(541, 155);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(389, 274);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lbresult
+            // 
+            this.lbresult.AutoSize = true;
+            this.lbresult.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbresult.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbresult.Location = new System.Drawing.Point(460, 155);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(56, 36);
+            this.lbresult.TabIndex = 8;
+            this.lbresult.Text = "0/4";
+            this.lbresult.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(449, 231);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 42);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbans);
+            this.Controls.Add(this.lbscore);
+            this.Controls.Add(this.lbquestion);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbquestion;
+        private System.Windows.Forms.Label lbscore;
+        private System.Windows.Forms.Label lbans;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbresult;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- Pig-Pig-Egg/Form1.cs
+++ Pig-Pig-Egg/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Pig_Pig_Egg
 {
@@ -15,6 +16,246 @@ namespace Pig_Pig_Egg
         public Form1()
         {
             InitializeComponent();
+        }
+        public int[] Pic_Codes;
+        public int[] Ans;
+        public int count;
+        public int score;
+        private delegate void UpdateUI();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.Load("pic/icon.png");
+            Init();
+        }
+        
+        public void Init()
+        {
+            count = 0;
+            score = 0;
+            Pic_Codes = NoRepeatRandomArray(4, 1, 5);
+            Ans = RandomArray(4, 0, 2);
+            string Hubble_path = "pic/Hubble_" + Pic_Codes[0] + ".png";
+            string Webb_path = "pic/Webb_" + Pic_Codes[0] + ".png";
+            if (Ans[0] == 0)
+            {
+                pictureBox1.Load(Webb_path);
+                pictureBox2.Load(Hubble_path);
+            }
+            else
+            {
+                pictureBox1.Load(Hubble_path);
+                pictureBox2.Load(Webb_path);
+            }
+        }
+
+        #region "Generate non-repeating random array"
+        public int[] NoRepeatRandomArray(int length, int min, int max)
+        {
+            int[] randomArray = new int[length];
+            Random crandom = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                randomArray[i] = crandom.Next(min, max);
+
+                for(int j = 0; j < i; j++)
+                {
+                    while(randomArray[j] == randomArray[i])
+                    {
+                        j = 0;
+                        randomArray[i] = crandom.Next(min, max);
+                    }
+                }
+            }                     
+            return randomArray;
+        }
+        #endregion
+
+        #region "Generate random array"
+        public int[] RandomArray(int length, int min, int max)
+        {
+            int[] randomArray = new int[length];
+            Random crandom = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                randomArray[i] = crandom.Next(min, max);
+            }
+            return randomArray;
+        }
+        #endregion
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Thread th1 = new Thread(LeftJudge);
+            th1.Start();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Thread th2 = new Thread(RightJudge);
+            th2.Start();
+        }
+
+        private void LeftJudge()
+        {
+            if (Ans[count] == 0)
+            {
+                Bingo();
+                Thread.Sleep(1000);
+                if (count != 3)
+                {
+                    NextQuestion();
+                }
+                else
+                {
+                    Result();
+                }
+            }
+            else
+            {
+                Wrong();
+                Thread.Sleep(1000);
+                if (count != 3)
+                {
+                    NextQuestion();
+                }
+                else
+                {
+                    Result();
+                }
+            }
+        }
+        private void RightJudge()
+        {
+            if (Ans[count] == 1)
+            {
+                Bingo();
+                Thread.Sleep(1000);
+                if (count != 3)
+                {
+                    NextQuestion();
+                }
+                else
+                {
+                    Result();
+                }
+            }
+            else
+            {
+                Wrong();
+                Thread.Sleep(1000);
+                if (count != 3)
+                {
+                    NextQuestion();
+                }
+                else
+                {
+                    Result();
+                }
+            }
+        }
+        private void Bingo()
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUI del = new UpdateUI(Bingo);
+                this.Invoke(del);
+            }
+            else
+            {
+                lbans.Visible = true;
+                score++;
+                lbans.Text = "Bingo!!!";
+                lbscore.Text = score + "/4";
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = false;
+
+            }
+        }
+
+        private void Wrong()
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUI del = new UpdateUI(Wrong);
+                this.Invoke(del);
+            }
+            else
+            {
+                lbans.Visible = true;
+                lbans.Text = "Wrong!!!";
+                lbscore.Text = score + "/4";
+                pictureBox1.Enabled = false;
+                pictureBox2.Enabled = false;
+            }
+        }
+
+        private void NextQuestion()
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUI del = new UpdateUI(NextQuestion);
+                this.Invoke(del);
+            }
+            else
+            {
+                pictureBox1.Enabled = true;
+                pictureBox2.Enabled = true;
+                lbans.Visible = false;
+                count++;
+                string Hubble_path = "pic/Hubble_" + Pic_Codes[count] + ".png";
+                string Webb_path = "pic/Webb_" + Pic_Codes[count] + ".png";
+                if (Ans[count] == 0)
+                {
+                    pictureBox1.Load(Webb_path);
+                    pictureBox2.Load(Hubble_path);
+                }
+                else
+                {
+                    pictureBox1.Load(Hubble_path);
+                    pictureBox2.Load(Webb_path);
+                }
+            }
+        }
+
+        private void Result()
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateUI del = new UpdateUI(Result);
+                this.Invoke(del);
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                pictureBox4.Visible = true;
+                lbans.Visible = false;
+                lbquestion.Visible = false;
+                lbscore.Visible = false;
+                lbresult.Visible = true;
+                lbresult.Text = score + "/4";
+                pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox4.Load("pic/restart.png");
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox4.Visible = false;
+            lbans.Visible = false;
+            lbquestion.Visible = true;
+            lbscore.Visible = true;
+            lbresult.Visible = false;
+            Init();
+            lbscore.Text = score + "/4";
+            lbans.Text = "";
         }
     }
 }
